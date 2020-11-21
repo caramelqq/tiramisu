e1d25362dd821f6e8aeba29a07b403551514299c
David Q <davquang@gmail.com>
2020-11-21 11:33:40 -0800

    added network return error checking

--- cogs/getviswax.py
+++ cogs/getviswax.py
@@ -31,41 +31,52 @@ class getviswax(commands.Cog):
 
     def get_combo_from_post(self, requests_text):
         soup = BeautifulSoup(requests_text, 'html.parser')
-        quoted_posts = str(soup.find_all(class_='quote')[0])
-        return re.findall(r'>(-\s*[^<]*)', quoted_posts)
+        try:
+            quoted_posts = str(soup.find_all(class_='quote')[0])
+            return re.findall(r'>(-\s*[^<]*)', quoted_posts)
+        except:
+            return None
+
         
     def get_date_from_post(self, requests_text):
         soup = BeautifulSoup(requests_text, 'html.parser')
-        quoted_posts = str(soup.find_all(class_='quote')[0])
-        return re.findall(r'Combination\sfor\s(\w*)\sthe\s(\d*)(\w*)', quoted_posts)
-
+        try:
+            quoted_posts = str(soup.find_all(class_='quote')[0])
+            return re.findall(r'Combination\sfor\s(\w*)\sthe\s(\d*)(\w*)', quoted_posts)
+        except:
+            return None
+            
     def post_vis_wax_combo(self):
-        # Check if we already have the result
-        if all(self.todays_combo) and self.todays_combo[0] == datetime.datetime.now().day:
-            return self.todays_combo
+        try:
+            # Check if we already have the result
+            if all(self.todays_combo) and self.todays_combo[0] == datetime.datetime.now().day:
+                return self.todays_combo
 
-        requests_response_text = self.get_forum_post()
-        # Check date - if post is outdated (date doesn't equal today), invalidate current combo and return nothing
-        d = self.get_date_from_post(requests_response_text)[0]
-        if int(d[1]) != datetime.datetime.now().day:
+            requests_response_text = self.get_forum_post()
+            # Check date - if post is outdated (date doesn't equal today), invalidate current combo and return nothing
+            d = self.get_date_from_post(requests_response_text)[0]
+            if int(d[1]) != datetime.datetime.now().day:
+                self.todays_combo = [None, None, None, None]
+                return self.todays_combo
+
+            title = 'Combination for ' + d[0] + ' the ' + d[1] + d[2]
+            slot_1 = ''
+            slot_2 = ''
+
+            s = self.get_combo_from_post(requests_response_text)
+            
+            for i in range(len(s) - 1):
+                if i == 0:
+                    slot_1 += s[i]
+                if i >= 1:
+                    slot_2 += s[i]
+                    slot_2 += '\n'
+
+            self.todays_combo = [int(d[1]), title, slot_1, slot_2]
+            return self.todays_combo
+        except:
             self.todays_combo = [None, None, None, None]
             return self.todays_combo
-
-        title = 'Combination for ' + d[0] + ' the ' + d[1] + d[2]
-        slot_1 = ''
-        slot_2 = ''
-
-        s = self.get_combo_from_post(requests_response_text)
-        
-        for i in range(len(s) - 1):
-            if i == 0:
-                slot_1 += s[i]
-            if i >= 1:
-                slot_2 += s[i]
-                slot_2 += '\n'
-
-        self.todays_combo = [int(d[1]), title, slot_1, slot_2]
-        return self.todays_combo
 
     @commands.command()
     async def vis(self, ctx):
